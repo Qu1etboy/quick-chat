2fca9235395975ba08767648abea981694070e9f
Qu1etboy <nonzagreanthai@gmail.com>
2024-01-27 15:21:31 +0700

    feat: connect server and client

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,23 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function Home() {
   const [name, setName] = useState<string>("");
+  const [roomName, setRoomName] = useState<string>("");
+  const [roomId, setRoomId] = useState<string>("");
 
   function handleSetName() {
     sessionStorage.setItem("myvalue", name);
@@ -15,13 +26,17 @@ export default function Home() {
 
   function joinRoom() {
     handleSetName();
-    window.location.href = "/rooms/1";
+    window.location.href = `/rooms/${roomId}`;
   }
 
-  function createRoom() {
+  async function createRoom() {
     handleSetName();
 
-    // window.location.href = "/rooms/2";
+    const room = await axios.post("http://localhost:4000/api/rooms", {
+      name: roomName,
+    });
+
+    window.location.href = `/rooms/${room.data.id}`;
   }
 
   return (
@@ -36,15 +51,50 @@ export default function Home() {
         onChange={(e) => setName(e.target.value)}
       />
       <div className="my-2 space-x-2">
-        <Button onClick={joinRoom} className="bg-blue-700 hover:bg-blue-800">
-          Join Room
-        </Button>
-        <Button
-          onClick={createRoom}
-          className="bg-green-700 hover:bg-green-800"
-        >
-          Create Room
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="bg-blue-700 hover:bg-blue-800">Join Room</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create a Room</DialogTitle>
+              {/* <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription> */}
+            </DialogHeader>
+            <Label>Room Id</Label>
+            <Input
+              value={roomId}
+              placeholder="Room id to join"
+              onChange={(e) => setRoomId(e.target.value)}
+            />
+            <Button onClick={joinRoom}>Join</Button>
+          </DialogContent>
+        </Dialog>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="bg-green-700 hover:bg-green-800">
+              Create Room
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create a Room</DialogTitle>
+              {/* <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription> */}
+            </DialogHeader>
+            <Label>Room Name</Label>
+            <Input
+              value={roomName}
+              placeholder="Your room name"
+              onChange={(e) => setRoomName(e.target.value)}
+            />
+            <Button onClick={createRoom}>Create Room</Button>
+          </DialogContent>
+        </Dialog>
       </div>
     </main>
   );
